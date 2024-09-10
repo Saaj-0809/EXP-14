309ec2cd7697a9f8b70c6fb22757329ef8a8f191
Saaj-0809 <saazmulik2005@gmail.com>
2024-09-11 03:05:29 +0530

    Add files via upload

---
+++ 14_1_Single Inheritance.cpp
@@ -0,0 +1,22 @@
+//Name: Saaj Mulik
+//Prn: 23070123109
+//Class: EnTC B-2
+#include<iostream>
+#include<string>
+using namespace std;
+class Uni{
+    public:
+    string uni= "Symbiosis";
+    void discipline(){
+        cout << "Engineering" << endl;
+    }
+};
+class Dep : public Uni{
+    public:
+    string dept= " Electronics & Telecommunication";
+};
+int main(){
+    Dep u1;
+    u1.discipline();
+    cout<<u1.uni + " "+u1.dept ;
+}
